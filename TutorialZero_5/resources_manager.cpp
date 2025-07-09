61464a008683cf3d879cd5735b3cdc8ced37c7bc
Aisakave <1021194976@qq.com>
2025-07-09 14:02:00 +0800

    fix util.h and resources_manager.cpp and add main.cpp

--- TutorialZero_5/resources_manager.cpp
+++ TutorialZero_5/resources_manager.cpp
@@ -2,6 +2,110 @@
 
 ResourceManager* ResourceManager::manager = nullptr;
 
+struct ImageResInfo
+{
+	std::string id;
+	LPCTSTR path;
+};
+
+struct AtlasResInfo
+{
+	std::string id;
+	LPCTSTR path;
+	int num_frame = 0;
+};
+
+// 添加图片信息列表的定义
+static const std::vector<ImageResInfo> image_info_list =
+{
+
+	// R标记表示原始字符串，就不用对字符串的反斜线进行转义处理了
+
+	{ "background",					_T(R"(resources\background.png)") },
+
+	{ "ui_heart",					_T(R"(resources\ui_heart.png)")},
+
+
+
+	{ "player_attack_right",		_T(R"(resources\player\attack.png)")},
+
+	{ "player_dead_right",			_T(R"(resources\player\dead.png)")},
+
+	{ "player_fall_right",			_T(R"(resources\player\fall.png)")},
+
+	{ "player_idle_right",			_T(R"(resources\player\idle.png)")},
+
+	{ "player_jump_right",			_T(R"(resources\player\jump.png)")},
+
+	{ "player_run_right",			_T(R"(resources\player\run.png)")},
+
+	{ "player_roll_right",			_T(R"(resources\player\roll.png)")},
+
+
+
+	{ "player_vfx_attack_down",		_T(R"(resources\player\vfx_attack_down.png)")},
+
+	{ "player_vfx_attack_left",		_T(R"(resources\player\vfx_attack_left.png)")},
+
+	{ "player_vfx_attack_right",	_T(R"(resources\player\vfx_attack_right.png)")},
+
+	{ "player_vfx_attack_up",		_T(R"(resources\player\vfx_attack_up.png)")},
+
+	{ "player_vfx_jump",			_T(R"(resources\player\vfx_jump.png)")},
+
+	{ "player_vfx_land",			_T(R"(resources\player\vfx_land.png)")},
+
+};
+
+// 添加图集信息列表的定义
+static const std::vector<AtlasResInfo> atlas_info_list =
+{
+
+	{ "barb_break",					_T(R"(resources\enemy\barb_break\%d.png)"),	3},
+
+	{ "barb_loose",					_T(R"(resources\enemy\barb_loose\%d.png)"),	5},
+
+	{ "silk",						_T(R"(resources\enemy\silk\%d.png)"),		9},
+
+	{ "sword_left",					_T(R"(resources\enemy\sword\%d.png)"),		3},
+
+
+
+	{ "enemy_aim_left",				_T(R"(resources\enemy\aim\%d.png)"),			9 },
+
+	{ "enemy_dash_in_air_left",		_T(R"(resources\enemy\dash_in_air\%d.png)"),	2 },
+
+	{ "enemy_dash_on_floor_left",	_T(R"(resources\enemy\dash_on_floor\%d.png)"),	2 },
+
+	{ "enemy_fall_left",			_T(R"(resources\enemy\fall\%d.png)"),			4 },
+
+	{ "enemy_idle_left",			_T(R"(resources\enemy\idle\%d.png)"),			6 },
+
+	{ "enemy_jump_left",			_T(R"(resources\enemy\jump\%d.png)"),			8 },
+
+	{ "enemy_run_left",				_T(R"(resources\enemy\run\%d.png)"),			8 },
+
+	{ "enemy_squat_left",			_T(R"(resources\enemy\squat\%d.png)"),			10},
+
+	{ "enemy_throw_barb_left",		_T(R"(resources\enemy\throw_barb\%d.png)"),		8 },
+
+	{ "enemy_throw_silk_left",		_T(R"(resources\enemy\throw_silk\%d.png)"),		17},
+
+	{ "enemy_throw_sword_left",		_T(R"(resources\enemy\throw_sword\%d.png)"),	16},
+
+
+
+	{ "enemy_vfx_dash_in_air_left",		_T(R"(resources\enemy\vfx_dash_in_air\%d.png)"),	5},
+
+	{ "enemy_vfx_dash_on_floor_left",	_T(R"(resources\enemy\vfx_dash_on_floor\%d.png)"),	6},
+
+};
+
+static inline bool check_image_valid(IMAGE* image)
+{
+	return GetImageBuffer(image);
+}
+
 ResourceManager* ResourceManager::get_instance()
 {
 	if (manager == nullptr)
@@ -143,18 +247,19 @@ IMAGE* ResourceManager::find_image(const std::string& id) const
 	return it->second;
 }
 
-static inline bool check_image_valid(IMAGE* image)
-{
-	return GetImageBuffer(image);
-}
+ResourceManager::ResourceManager() = default;
 
+ResourceManager::~ResourceManager() = default;
 
-void ResourceManager::flip_image(IMAGE* src_image, IMAGE* dst_image, int num_h = 1)
+// 定义处不带默认值默认参数只能在声明（.h）文件中指定一次, 所以这里不能写成 int num_h = 1
+// 在定义（.cpp）文件中再写默认参数会被认为是“重定义”，导致 C2572 编译错误
+
+void ResourceManager::flip_image(IMAGE* src_image, IMAGE* dst_image, int num_h)
 {
 	int w = src_image->getwidth();
 	int h = src_image->getheight();
 	int w_frame = w / num_h;
-	Resize(dst_image, w_frame, h);
+	Resize(dst_image, w, h); // 注意是w
 	DWORD* src_buffer = (DWORD*)GetImageBuffer(src_image);
 	DWORD* dst_buffer = (DWORD*)GetImageBuffer(dst_image);
 	for (int i = 0; i < num_h; i++)
@@ -173,7 +278,7 @@ void ResourceManager::flip_image(IMAGE* src_image, IMAGE* dst_image, int num_h =
 	}
 }
 
-void ResourceManager::flip_image(const std::string& src_id, const std::string& dst_id, int num_h = 1)
+void ResourceManager::flip_image(const std::string& src_id, const std::string& dst_id, int num_h)
 {
 	IMAGE* src_image = image_pool[src_id];
 	IMAGE* dst_image = new IMAGE();
@@ -197,109 +302,7 @@ void ResourceManager::flip_atlas(const std::string& src_id, const std::string& d
 	atlas_pool[dst_id] = dst_atlas;
 }
 
-ResourceManager::ResourceManager() = default;
-
-ResourceManager::~ResourceManager() = default;
-
-
-struct ImageResInfo 
-{
-	std::string id;
-	LPCTSTR path;
-};
-
-struct AtlasResInfo 
-{
-	std::string id;
-	LPCTSTR path;
-	int num_frame = 0;
-};
-
-// 添加图片信息列表的定义
-static const std::vector<ImageResInfo> image_info_list =
-{
-
-	// R标记表示原始字符串，就不用对字符串的反斜线进行转义处理了
-
-	{ "background",					_T(R"(resources\background.png)") },
-
-	{ "ui_heart",					_T(R"(resources\ui_heart.png)")},
 
 
 
-	{ "player_attack_right",		_T(R"(resources\player\attack.png)")},
-
-	{ "player_dead_right",			_T(R"(resources\player\dead.png)")},
-
-	{ "player_fall_right",			_T(R"(resources\player\fall.png)")},
-
-	{ "player_idle_right",			_T(R"(resources\player\idle.png)")},
-
-	{ "player_jump_right",			_T(R"(resources\player\jump.png)")},
-
-	{ "player_run_right",			_T(R"(resources\player\run.png)")},
-
-	{ "player_roll_right",			_T(R"(resources\player\roll.png)")},
-
-
-
-	{ "player_vfx_attack_down",		_T(R"(resources\player\vfx_attack_down.png)")},
-
-	{ "player_vfx_attack_left",		_T(R"(resources\player\vfx_attack_left.png)")},
-
-	{ "player_vfx_attack_right",	_T(R"(resources\player\vfx_attack_right.png)")},
-
-	{ "player_vfx_attack_up",		_T(R"(resources\player\vfx_attack_up.png)")},
-
-	{ "player_vfx_jump",			_T(R"(resources\player\vfx_jump.png)")},
-
-	{ "player_vfx_land",			_T(R"(resources\player\vfx_land.png)")},
-
-};
-
-
-
-// 添加图集信息列表的定义
-static const std::vector<AtlasResInfo> atlas_info_list = 
-{
-
-	{ "barb_break",					_T(R"(resources\enemy\barb_break\%d.png)"),	3},
-
-	{ "barb_loose",					_T(R"(resources\enemy\barb_loose\%d.png)"),	5},
-
-	{ "silk",						_T(R"(resources\enemy\silk\%d.png)"),		9},
-
-	{ "sword_left",					_T(R"(resources\enemy\sword\%d.png)"),		3},
-
-
-
-	{ "enemy_aim_left",				_T(R"(resources\enemy\aim\%d.png)"),			9 },
-
-	{ "enemy_dash_in_air_left",		_T(R"(resources\enemy\dash_in_air\%d.png)"),	2 },
-
-	{ "enemy_dash_on_floor_left",	_T(R"(resources\enemy\dash_on_floor\%d.png)"),	2 },
-
-	{ "enemy_fall_left",			_T(R"(resources\enemy\fall\%d.png)"),			4 },
-
-	{ "enemy_idle_left",			_T(R"(resources\enemy\idle\%d.png)"),			6 },
-
-	{ "enemy_jump_left",			_T(R"(resources\enemy\jump\%d.png)"),			8 },
-
-	{ "enemy_run_left",				_T(R"(resources\enemy\run\%d.png)"),			8 },
-
-	{ "enemy_squat_left",			_T(R"(resources\enemy\squat\%d.png)"),			10},
-
-	{ "enemy_throw_barb_left",		_T(R"(resources\enemy\throw_barb\%d.png)"),		8 },
-
-	{ "enemy_throw_silk_left",		_T(R"(resources\enemy\throw_silk\%d.png)"),		17},
-
-	{ "enemy_throw_sword_left",		_T(R"(resources\enemy\throw_sword\%d.png)"),	16},
-
-
-
-	{ "enemy_vfx_dash_in_air_left",		_T(R"(resources\enemy\vfx_dash_in_air\%d.png)"),	5},
-
-	{ "enemy_vfx_dash_on_floor_left",	_T(R"(resources\enemy\vfx_dash_on_floor\%d.png)"),	6},
-
-};
 
--- TutorialZero_5/util.h
+++ TutorialZero_5/util.h
@@ -13,7 +13,7 @@ struct Rect
 // 裁剪img任意大小的图并显示在任意处
 	// src	裁剪
 	// dst	缩放	
-void putimage_ex(IMAGE* img, const Rect* rect_dst, const Rect* rect_src = nullptr)
+inline void putimage_ex(IMAGE* img, const Rect* rect_dst, const Rect* rect_src = nullptr)
 {
 
 	Rect src_{ 0, 0, img->getwidth(), img->getheight() };
@@ -49,3 +49,38 @@ inline void stop_audio(LPCTSTR id)
 	_stprintf_s(str_cmd, _T("stop %s"), id);
 	mciSendString(str_cmd, nullptr, 0, nullptr);
 }
+
+/*
+一、inline 的本意：内联函数
+最早 inline 的设计目的是让函数的代码在编译时被“展开”到调用处，而不是像普通函数那样跳转调用，目的是提高执行效率，尤其用于短小函数：
+inline int add(int a, int b) {
+	return a + b;
+}
+如果你写了：
+int x = add(2, 3);
+编译器可能就会直接变成：
+int x = 2 + 3;  // 编译器内联展开了函数
+不过现在现代编译器会自动决定是否内联，你写了 inline 它不一定真的内联，你不写它也可能会内联，所以：
+
+inline 的实际用处已经不是为了“展开调用”，而是为了-> “允许在头文件中定义函数”。
+
+二、为什么头文件中定义函数会报错？
+因为如果你在.h 文件里直接写函数实现：
+// utils.h
+void say_hi() {
+	std::cout << "hi\n";
+}
+如果多个.cpp 文件都 #include "utils.h"，那么 say_hi 会在多个.obj 文件中各生成一份定义，链接器报错：
+LNK2005 : 函数重复定义
+
+三、什么时候需要加 inline？
+在头文件中定义函数实现时加 inline，避免重复定义错误。
+// utils.h
+#pragma once
+#include <iostream>
+
+inline void say_hi() {
+	std::cout << "hi\n";
+}
+这样即使多个.cpp 文件包含这个头文件，也不会重复定义。
+*/
